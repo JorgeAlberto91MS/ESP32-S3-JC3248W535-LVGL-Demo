4dee2f4b1409dab914df74d9ae15587bc02fc0e6
Jorge Alberto Martínez Suárez <JorgeAlberto91MS@gmail.com>
2026-02-05 14:05:29 -0500

    Update ESP32-S3-JC3248W535-LVGL-Demo.ino

--- ESP32-S3-JC3248W535-LVGL-Demo.ino
+++ ESP32-S3-JC3248W535-LVGL-Demo.ino
@@ -3,26 +3,6 @@
 #include "src/esp_bsp.h"
 #include "src/display.h"
 
-// 1. Declaramos la función de eventos igual que en la documentación de LVGL
-static void btn_event_handler(lv_event_t * e) {
-    lv_event_code_t code = lv_event_get_code(e);
-    lv_obj_t * btn = lv_event_get_target(e); // Obtenemos el objeto que disparó el evento
-
-    if(code == LV_EVENT_CLICKED) {
-        static uint8_t cnt = 0;
-        cnt++;
-
-        // Obtenemos el primer hijo del botón (que es el label) para cambiar su texto
-        lv_obj_t * label = lv_obj_get_child(btn, 0);
-        lv_label_set_text_fmt(label, "Clics: %d", cnt);
-        
-        Serial.println("Evento: Click detectado");
-    }
-    else if(code == LV_EVENT_VALUE_CHANGED) {
-        Serial.println("Evento: Valor cambiado (Toggle)");
-    }
-}
-
 void setup() {
     Serial.begin(115200);
 
@@ -51,7 +31,7 @@ void setup_main_screen() {
     lv_obj_center(btn);
     
     // Si quieres que el botón sea tipo "Toggle" (se quede hundido), descomenta esta línea:
-    // lv_obj_add_flag(btn, LV_OBJ_FLAG_CHECKABLE);
+     lv_obj_add_flag(btn, LV_OBJ_FLAG_CHECKABLE);
 
     /* ASIGNACIÓN DEL EVENTO (Igual que en la documentación) */
     lv_obj_add_event_cb(btn, btn_event_handler, LV_EVENT_ALL, NULL);
@@ -63,4 +43,24 @@ void setup_main_screen() {
 
 void loop() {
     delay(1000);
+}
+
+// 1. Declaramos la función de eventos igual que en la documentación de LVGL
+static void btn_event_handler(lv_event_t * e) {
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * btn = lv_event_get_target(e); // Obtenemos el objeto que disparó el evento
+
+    if(code == LV_EVENT_CLICKED) {
+        static uint8_t cnt = 0;
+        cnt++;
+
+        // Obtenemos el primer hijo del botón (que es el label) para cambiar su texto
+        lv_obj_t * label = lv_obj_get_child(btn, 0);
+        lv_label_set_text_fmt(label, "Clics: %d", cnt);
+        
+        Serial.println("Evento: Click detectado");
+    }
+    else if(code == LV_EVENT_VALUE_CHANGED) {
+        Serial.println("Evento: Valor cambiado (Toggle)");
+    }
 }
